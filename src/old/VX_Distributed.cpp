422bce1cc8bde2a9fb6a25efe5daa0fd363973cf
Federico Pigozzi <pigozzife@gmail.com>
2022-05-26 17:09:20 -0400

    refactor controller

--- src/old/VX_Distributed.cpp
+++ src/old/VX_Distributed.cpp
@@ -5,13 +5,13 @@
 #include <stdlib.h>
 #include <cstdlib>
 
-CVX_Distributed::CVX_Distributed(const int numInputs, const int numMaterials, const std::string weights)
+CVX_Distributed::CVX_Distributed(const int numInputs, const int numVoxels, const std::string weights)
 {
-  this->numMaterials = numMaterials;
+  this->numVoxels = numVoxels;
   mlp = new CVX_MLP(numInputs, 6, weights);
-  lastSignals = (double**) malloc(sizeof(double*) * numMaterials);
-  currSignals = (double**) malloc(sizeof(double*) * numMaterials);
-  for (int i = 0; i < numMaterials; ++i) {
+  lastSignals = (double**) malloc(sizeof(double*) * numVoxels);
+  currSignals = (double**) malloc(sizeof(double*) * numVoxels);
+  for (int i = 0; i < numVoxels; ++i) {
     lastSignals[i] = (double*) malloc(sizeof(double) * 4);
     currSignals[i] = (double*) malloc(sizeof(double) * 4);
     //std::fill(lastSignals[i], lastSignals[i] + 4, 0.0);
@@ -24,7 +24,7 @@ CVX_Distributed::CVX_Distributed(const int numInputs, const int numMaterials, co
 
 CVX_Distributed::~CVX_Distributed(void)
 {
-  for (int i = 0; i < numMaterials; ++i) {
+  for (int i = 0; i < numVoxels; ++i) {
     delete lastSignals[i];
     delete currSignals[i];
   }
@@ -33,35 +33,44 @@ CVX_Distributed::~CVX_Distributed(void)
   delete mlp;
 }
 
-void CVX_Distributed::UpdateMatTemp(CVX_Object* pObjUpdate, double TempBase)
+double CVX_Distributed::UpdateMatTemp(CVX_Object* pObj, CVX_Voxel* voxel)
 {
-  for (int i = 0; i < (int)pObjUpdate->GetNumMaterials(); ++i) {
+  //for (int i = 0; i < (int)pObjUpdate->GetNumMaterials(); ++i) {
     double* sensors = (double*) malloc(sizeof(double));
-    sensors[0] = pObjUpdate->GetBaseMat(i)->GetCurMatTemp();
-    double* signals = GetLastSignals(i, pObjUpdate);
+    sensors[0] = voxel->temp;//pObjUpdate->GetBaseMat(i)->GetCurMatTemp();
+    double* signals = GetLastSignals(voxel, pObj);
     double* inputs = new double[mlp->getNumInputs()];
     std::copy(sensors, sensors + mlp->getNumInputs() - 4, inputs);
     std::copy(signals, signals + 4, inputs + mlp->getNumInputs() - 4);
     double* outputs = mlp->Apply(inputs);
-    pObjUpdate->GetBaseMat(i)->SetCurMatTemp(TempBase + outputs[0]);
+    //pObjUpdate->GetBaseMat(i)->SetCurMatTemp(TempBase + outputs[0]);
     std::cout << i << ": ";
     for (int k = 0; k < 6; ++k) {
       std::cout << outputs[k] << " ";
     }
     std::cout << std::endl;
     std::copy(outputs + 2, outputs + mlp->getNumOutputs(), currSignals[i]);
-  }
-  for (int i = 0; i < (int)pObjUpdate->GetNumMaterials(); ++i) {
+  //}
+  return outputs[0];
+}
+
+void CVX_Distributed::UpdateSignals(void)
+{
+  for (int i = 0; i < numVoxels; ++i) {
     std::copy(currSignals[i], currSignals[i] + 4, lastSignals[i]);
   }
 }
 
-double* CVX_Distributed::GetLastSignals(int i, CVX_Object* pObjUpdate) const
+double* CVX_Distributed::GetLastSignals(CVX_Voxel* voxel, CVX_Object* pObj) const
 {
   double* signals = (double*) malloc(sizeof(double) * 4);
-  Vec3D<> currPoint;
-  pObjUpdate->GetXYZ(&currPoint, i);
-  int idx = pObjUpdate->GetIndex(currPoint.x + 1, currPoint.y, currPoint.z);
+  //Vec3D<>* currPoint = new Vec3D<>(voxel->ix, voxel->iy, voxel->iz);
+  //pObjUpdate->GetXYZ(&currPoint, i);
+  for (int dir = 0; dir < 4; ++dir) {
+    CVX_Voxel* adjVoxel = voxel->adjacentVoxel(dir);
+    signals[dir] = lastSignals[pObj->GetIndex(adjVoxel->ix, adjVoxel->iy, adjVoxel->iz)];
+  }
+  /*int idx = pObjUpdate->GetIndex(currPoint.x + 1, currPoint.y, currPoint.z);
   if (idx != -1 && pObjUpdate->Structure.GetData(idx) != 0) {
     signals[0] = lastSignals[idx][0];
   }
@@ -88,6 +97,6 @@ double* CVX_Distributed::GetLastSignals(int i, CVX_Object* pObjUpdate) const
   }
   else {
     signals[3] = 0.0;
-  }
+  }*/
   return signals;
 }
