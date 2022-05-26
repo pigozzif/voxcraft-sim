16511b4370c76b5b215ceb38141ab23852d9f362
Federico Pigozzi <pigozzife@gmail.com>
2022-05-26 17:14:26 -0400

    refactor controller

--- src/old/VX_Distributed.h
+++ src/old/VX_Distributed.h
@@ -3,19 +3,21 @@
 
 #include "VX_MLP.h"
 #include "VX_Object.h"
+#include "VX_Voxel.h"
 #include <string>
 
 class CVX_Distributed
 {
 public:
-  CVX_Distributed(const int numInputs, const int numMaterials, const std::string weights);
+  CVX_Distributed(const int numInputs, const int numVoxels, const std::string weights);
   ~CVX_Distributed(void);
 
-  void UpdateMatTemp(CVX_Object* pObjUpdate, double TempBase);
+  void UpdateMatTemp(CVX_Object* pObjUpdate, CVX_Voxel* voxel);
+  void UpdateLastSignals(void);
   double* GetLastSignals(int i, CVX_Object* pObjUpdate) const;
 
 private:
-  int numMaterials;
+  int numVoxels;
   CVX_MLP* mlp;
   double** lastSignals;
   double** currSignals;
