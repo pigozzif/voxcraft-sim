c753abcf80d7e9d25ab9ca0923effe74a404fd9e
Federico Pigozzi <pigozzife@gmail.com>
2022-05-26 18:57:05 -0400

    debug

--- src/old/VX_Environment.cpp
+++ src/old/VX_Environment.cpp
@@ -349,7 +349,7 @@ float CVX_Environment::UpdateCurTemp(vfloat time, CVX_Object* pUpdateInObj, CVX_
 	if (VaryTempEnabled && voxel->matid == 1){
 		if (TempPeriod == 0) return 0.0f; //avoid NaNs.
 		//CurTemp = TempBase + TempAmplitude*sin(2*3.1415926/TempPeriod*time);	//update the global temperature
-		//CurTemp = TempBase + TempAmplitude*Controller->UpdateVoxelTemp(pObjUpdate, voxel);
+		CurTemp = TempBase + TempAmplitude*Controller->UpdateVoxelTemp(pObjUpdate, voxel);
 		//for (int i = 0; i<(int)pObjUpdate->GetNumMaterials(); i++){ //now update the individual temperatures of each material (they can each have a different temperature)
 		//	pObjUpdate->GetBaseMat(i)->SetCurMatTemp(TempBase + TempAmplitude*sin((2*3.1415926f/TempPeriod) * time + pObjUpdate->GetBaseMat(i)->GetMatTempPhase()));	//and update each one
 		//}
