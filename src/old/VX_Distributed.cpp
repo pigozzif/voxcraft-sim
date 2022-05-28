ee15a323b52c770dba4d2d5ba8dd92dd11bda284
Federico Pigozzi <pigozzife@gmail.com>
2022-05-28 13:26:38 -0400

    debug

--- src/old/VX_Distributed.cpp
+++ src/old/VX_Distributed.cpp
@@ -44,8 +44,8 @@ double CVX_Distributed::UpdateVoxelTemp(CVX_Object* pObj, CVX_Voxel* voxel)
     double* sensors = (double*) malloc(sizeof(double) * 6);
   for (int i = 0; i < 6; ++i) {
     Vec3D<double>* offset = touchSensor->getOffset((CVX_Voxel::linkDirection)i);
-    std::cout << voxel->pos.x << voxel->pos.y << voxel->pos.z << offset->x << offset->y << offset->z << std::endl;
-    sensors[i] = touchSensor->sense(voxel, voxel/*sim->voxel((int)voxel->pos.x + offset->x, (int)voxel->pos.y + offset->y, (int)voxel->pos.z + offset->z)*/, (CVX_Voxel::linkDirection)i);//voxel->temp;//pObjUpdate->GetBaseMat(i)->GetCurMatTemp();
+    //std::cout << voxel->pos.x << voxel->pos.y << voxel->pos.z << offset->x << offset->y << offset->z << std::endl;
+    sensors[i] = touchSensor->sense(voxel, sim->voxel(voxel->ix + offset->x, voxel->iy + offset->y, voxel->iz + offset->z), (CVX_Voxel::linkDirection)i);//voxel->temp;//pObjUpdate->GetBaseMat(i)->GetCurMatTemp();
   }
   sensors[5] = (voxel->floorPenetration() >= 0) ? 1.0 : -1.0;
   
