28c2588bfa43934a6a428418f0694c6b6331b66d
Federico Pigozzi <pigozzife@gmail.com>
2022-05-31 11:30:15 -0400

    debug

--- src/old/VX_Distributed.cpp
+++ src/old/VX_Distributed.cpp
@@ -123,10 +123,11 @@ CVX_TouchSensor::~CVX_TouchSensor(void) {}
 
 double CVX_TouchSensor::sense(CVX_Voxel* source, CVX_Voxel* target, CVX_Voxel::linkDirection dir) const
 {
+  std::cout << "check:" << " (" << target->matid << ") " << std::endl;
   if (!target || target->matid == 0/* || target == source->adjacentVoxel(dir)*/) {
     return -1.0;
   }
-  std::cout << "we are here" << " (" << target->ix << "," << target->iy << "," << target->iz << ") " << std::endl;
+  std::cout << "we are here:" << " (" << target->ix << "," << target->iy << "," << target->iz << ") " << std::endl;
   linkAxis axis = CVX_Voxel::toAxis(dir);
   double baseSize = source->baseSize(axis);
   bool isPositive = CVX_Voxel::isPositive(dir);
