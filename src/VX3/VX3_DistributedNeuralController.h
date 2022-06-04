d612639f4dbca5f4bccfc0f2f88bc7cd315e60cf
Federico Pigozzi <pigozzife@gmail.com>
2022-06-04 12:44:17 -0400

    back with headers

--- src/VX3/VX3_DistributedNeuralController.h
+++ src/VX3/VX3_DistributedNeuralController.h
@@ -2,12 +2,11 @@
 #define VX3_DISTRIBUTED_H
 
 #include "VX3.cuh"
+#include "VX3_Voxel.h"
+#include "VX3_VoxelyzeKernel.cuh"
 
 #define NUM_SENSORS 6
 #define NUM_SIGNALS 6
-
-class VX3_Voxel;
-class VX3_VoxelyzeKernel;
 
 class VX3_MLP
 {
