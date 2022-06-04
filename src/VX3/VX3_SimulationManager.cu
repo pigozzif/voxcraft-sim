e031abfeca3c23330a9c83cc2ed3383c4b8ced54
Federico Pigozzi <pigozzife@gmail.com>
2022-06-04 12:07:39 -0400

    fix typo

--- src/VX3/VX3_SimulationManager.cu
+++ src/VX3/VX3_SimulationManager.cu
@@ -13,7 +13,7 @@ __global__ void CUDA_Simulation(VX3_VoxelyzeKernel *d_voxelyze_3, int num_simula
     int thread_index = blockIdx.x * blockDim.x + threadIdx.x;
     if (thread_index < num_simulation) {
         VX3_VoxelyzeKernel *d_v3 = &d_voxelyze_3[thread_index];
-        VX3_NeuralDistributedController* controller = new VX3_NeuralDistributedController();
+        VX3_DistributedNeuralController* controller = new VX3_DistributedNeuralController();
         controller->init(weights, d_v3);
         if (d_v3->num_d_links == 0 and d_v3->num_d_voxels == 0) {
             printf(COLORCODE_BOLD_RED "No links and no voxels. Simulation %d (%s) abort.\n" COLORCODE_RESET, thread_index,
