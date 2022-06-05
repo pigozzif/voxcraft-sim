345335d0d8402a5448909b22bc8d2349cd66ef99
Federico Pigozzi <pigozzife@gmail.com>
2022-06-05 16:43:22 -0400

    fix bug

--- src/VX3/VX3_SimulationManager.cu
+++ src/VX3/VX3_SimulationManager.cu
@@ -449,8 +449,8 @@ void VX3_SimulationManager::startKernel(int num_simulation, int device_index) {
     //             cudaMemcpyDeviceToHost);
     enlargeGPUHeapSize();
     enlargeGPUPrintfFIFOSize();
-    std::cout << "we are before allocation" << std::endl;
-    double** d_weights = readWeights(NUM_SENSORS + NUM_SIGNALS, NUM_SIGNALS + 2);
+    double** d_weights;
+    readWeights(d_weights, NUM_SENSORS + NUM_SIGNALS, NUM_SIGNALS + 2);
     std::cout << "we are here after allocation" << std::endl;
     CUDA_Simulation<<<numBlocks, threadsPerBlock>>>(d_voxelyze_3s[device_index], num_simulation, device_index, d_weights);
     CUDA_CHECK_AFTER_CALL();
