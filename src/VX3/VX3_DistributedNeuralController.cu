1bcf4225ef908b8446c796f4559cdb7aabdb6c04
Federico Pigozzi <pigozzife@gmail.com>
2022-06-05 18:06:06 -0400

    debug

--- src/VX3/VX3_DistributedNeuralController.cu
+++ src/VX3/VX3_DistributedNeuralController.cu
@@ -23,7 +23,7 @@ __device__ VX3_MLP::VX3_MLP(const int numInputs, const int numOutputs, double* w
   VcudaMalloc((void**) &outputs, sizeof(double) * numOutputs);
   VcudaMalloc((void**) &inputs, sizeof(double) * numInputs);
   printf("we are here\n");
-  for (int i = 0; i < numOutputs * (numInputs + 1); ++i) printf("%f ", weights[i]);
+  //for (int i = 0; i < numOutputs * (numInputs + 1); ++i) printf("%f ", weights[i]);
   this->weights = weights;
   //setWeights("");
 }
