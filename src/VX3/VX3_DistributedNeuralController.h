9979f2c794031146873cb1c4dd81bbe79dea2bb6
Federico Pigozzi <pigozzife@gmail.com>
2022-06-05 19:28:53 -0400

    missing *

--- src/VX3/VX3_DistributedNeuralController.h
+++ src/VX3/VX3_DistributedNeuralController.h
@@ -45,7 +45,7 @@ public:
   __device__ VX3_Vec3D<float>* getOffset(const linkDirection dir) const;
 
   VX3_MLP* mlp;
-  VX3_dVector<double> votes;
+  VX3_dVector<double>* votes;
 };
 
 #endif //VX3_DISTRIBUTED_H
