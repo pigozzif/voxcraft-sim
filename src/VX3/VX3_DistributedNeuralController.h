40c715f175c373a3514c1cefe8404adc22dea3c2
Federico Pigozzi <pigozzife@gmail.com>
2022-06-05 17:57:34 -0400

    fix typo

--- src/VX3/VX3_DistributedNeuralController.h
+++ src/VX3/VX3_DistributedNeuralController.h
@@ -19,7 +19,7 @@ public:
   __device__ inline int getNumInputs(void) const { return numInputs; }
   __device__ inline int getNumOutputs(void) const { return numOutputs; }
 
-  __device__ double** getWeights(void) const { return weights; };
+  __device__ double* getWeights(void) const { return weights; };
   __device__ void setWeights(char* weights);
   
   double* inputs;
