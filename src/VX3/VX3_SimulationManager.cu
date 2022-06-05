1e1de84bce29acbf43849030fd1ded967398e97e
Federico Pigozzi <pigozzife@gmail.com>
2022-06-05 16:54:40 -0400

    fix typo

--- src/VX3/VX3_SimulationManager.cu
+++ src/VX3/VX3_SimulationManager.cu
@@ -448,7 +448,7 @@ void VX3_SimulationManager::startKernel(int num_simulation, int device_index) {
     int numInputs = NUM_SENSORS + NUM_SIGNALS;
     d_weights = (double**) malloc(sizeof(double*) * numOutputs);
     for (int i = 0; i < numOutputs; ++i) {
-      w_weights[i] = (double**) malloc(sizeof(double) * (numInputs + 1));
+      d_weights[i] = (double**) malloc(sizeof(double) * (numInputs + 1));
     }
     readWeights(d_weights, numInputs, numOutputs);
     std::cout << "we are here after allocation" << std::endl;
