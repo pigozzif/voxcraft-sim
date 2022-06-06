a7d19307d22fc7228706a870f36592881e7cdc38
Federico Pigozzi <pigozzife@gmail.com>
2022-06-05 20:30:52 -0400

    fix bugs

--- src/VX3/VX3_VoxelyzeKernel.cu
+++ src/VX3/VX3_VoxelyzeKernel.cu
@@ -533,11 +533,11 @@ __device__ VX3_MaterialLink *VX3_VoxelyzeKernel::combinedMaterial(VX3_MaterialVo
 
 __device__ void VX3_VoxelyzeKernel::computeFitness(VX3_DistributedNeuralController* controller, VX3_Vec3D<double> d_max, int is_passable) {
     VX3_Vec3D<> offset = currentCenterOfMass - initialCenterOfMass;
-    double distance = sqrt(pow(offset.x - d_max.x, 2) + pow(offset.y - d_max.y, 2)); + //VX3_MathTree::eval(offset.x, offset.y, offset.z, collisionCount, currentTime, recentAngle, targetCloseness,
+    double distance = sqrt(pow(offset.x - d_max.x, 2) + pow(offset.y - d_max.y, 2)); //VX3_MathTree::eval(offset.x, offset.y, offset.z, collisionCount, currentTime, recentAngle, targetCloseness,
                                      //  numClosePairs, num_d_voxels, fitness_function);
     double voting = 0.0;
     for (int i = 0; i < controller->votes.size(); ++i) {
-      voting += controllers->votes == is_passable;
+      voting += controllers->votes->get(i) == is_passable;
     }
     voting /= controller->votes.size();
     fitness_score = distance + voting;
