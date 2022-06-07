42b271f72add87e1c2b663aa1db5e6fd7c81daaf
Federico Pigozzi <pigozzife@gmail.com>
2022-06-07 12:38:59 -0400

    fix bugs

--- src/VX3/VX3_SimulationManager.cu
+++ src/VX3/VX3_SimulationManager.cu
@@ -117,14 +117,14 @@ __global__ void CUDA_Simulation(VX3_VoxelyzeKernel *d_voxelyze_3, int num_simula
                 }
             }
         }
-        for (int i = 0; i < num_d_voxels; ++i) {
-          VX3_Voxel* voxel = d_voxels + i;
+        for (int i = 0; i < d_v_3->num_d_voxels; ++i) {
+          VX3_Voxel* voxel = d_v_3->d_voxels + i;
           if (voxel->matid == 3) {
-            target = voxel;
+            d_v_3->target = voxel;
             break;
           }
           else if (i == num_d_voxels - 1) { // horrible, just for debugging
-            target = voxel;
+            d_v_3->target = voxel;
           }
         }
         d_v3->updateCurrentCenterOfMass();
