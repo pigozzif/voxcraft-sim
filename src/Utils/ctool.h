3d26db3be0de0694965260cc96204e67c0c1f0c2
Federico Pigozzi <pigozzife@gmail.com>
2022-06-15 15:18:43 -0400

    fix bug

--- src/Utils/ctool.h
+++ src/Utils/ctool.h
@@ -26,7 +26,7 @@ namespace ctool {
         auto in_time_t = std::chrono::system_clock::to_time_t(now);
 
         std::stringstream folderName;
-        //folderName << std::put_time(std::localtime(&in_time_t), format.c_str());
+        folderName << std::put_time(std::localtime(&in_time_t), format.c_str());
         return folderName.str();
     }
 
