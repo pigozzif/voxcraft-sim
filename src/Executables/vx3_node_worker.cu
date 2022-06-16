81c5234111a10da5745a1cf7f2de2550f7728e9b
Federico Pigozzi <pigozzife@gmail.com>
2022-06-16 17:22:26 -0400

    fix filename

--- src/Executables/vx3_node_worker.cu
+++ src/Executables/vx3_node_worker.cu
@@ -104,7 +104,7 @@ int main(int argc, char** argv) {
 
     // this will be too much to write into the report.
     for (auto &res: sm.h_results) {
-        std::string simulation_name = split(res.vxa_filename, '.')[0];
+        std::string simulation_name = split(res.vxd_filename, '.')[0];
         tr_result.put("report.detail."+simulation_name+".currentTime", res.currentTime);
         tr_result.put("report.detail."+simulation_name+".fitness_score", res.fitness_score);
         tr_result.put("report.detail."+simulation_name+".num_voxel", res.num_voxel);
