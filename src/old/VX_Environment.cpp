13d860126d71aaf86eb1d902aaa19d49c52373a0
Federico Pigozzi <pigozzife@gmail.com>
2022-06-20 17:51:25 -0400

    debug

--- src/old/VX_Environment.cpp
+++ src/old/VX_Environment.cpp
@@ -183,6 +183,7 @@ bool CVX_Environment::ReadXML(CXML_Rip* pXML, std::string* RetMessage) //pXML po
 	
 	if (pXML->FindElement("Controller")){
 	        if (!pXML->FindLoadElement("NeuralWeights", &Weights)) Weights = "";
+		std::cout << Weights << std::endl;
 		pXML->UpLevel();
 	}
 	
