73fef266f9a2a19e149ffebe6a28750489e88d46
Federico Pigozzi <pigozzife@gmail.com>
2022-06-23 17:08:53 -0400

    fix fitness bug

--- src/VX3/VX3_VoxelyzeKernel.cu
+++ src/VX3/VX3_VoxelyzeKernel.cu
@@ -532,8 +532,9 @@ __device__ VX3_MaterialLink *VX3_VoxelyzeKernel::combinedMaterial(VX3_MaterialVo
 }
 
 __device__ void VX3_VoxelyzeKernel::computeFitness(VX3_DistributedNeuralController* controller, int is_passable) {
-    locomotion_score = 1.0 - sqrt(pow(currentCenterOfMass.x - target->pos.x, 2) + pow(currentCenterOfMass.y - target->pos.y, 2));
+    locomotion_score = sqrt(pow(currentCenterOfMass.x - target->pos.x, 2) + pow(currentCenterOfMass.y - target->pos.y, 2));
     locomotion_score /= sqrt(pow(initialCenterOfMass.x - target->pos.x, 2) + pow(initialCenterOfMass.y - target->pos.y, 2));
+    locomotion_score = 1.0 - locomotion_score;
     for (int i = 0; i < controller->votes->size(); ++i) {
       sensing_score += controller->votes->get(i) == is_passable;
     }
