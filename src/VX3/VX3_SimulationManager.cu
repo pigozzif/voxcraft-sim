a54a40e538082b6f1925c9da3f5f8d7b4b195648
Federico Pigozzi <pigozzife@gmail.com>
2022-09-07 14:39:50 -0400

    clear clear collisions

--- src/VX3/VX3_SimulationManager.cu
+++ src/VX3/VX3_SimulationManager.cu
@@ -81,7 +81,6 @@ __global__ void CUDA_Simulation(VX3_VoxelyzeKernel *d_voxelyze_3, int num_simula
         for (int j = 0; j < 1000000; j++) { // Maximum Steps 1000000
             if (d_v3->StopConditionMet())
                 break;
-            d_v3->d_v_collisions.clear();
             if (!d_v3->doTimeStep(controller)) {
                 printf(COLORCODE_BOLD_RED "\n%d) Simulation %d Diverged: %s.\n" COLORCODE_RESET, device_index, thread_index,
                        d_v3->vxa_filename);
