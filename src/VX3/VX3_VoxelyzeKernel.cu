71bc8b4ce31d7e6ccb527d924a9bb13eebb4f1df
Federico Pigozzi <pigozzife@gmail.com>
2022-09-27 18:01:05 -0400

    debug

--- src/VX3/VX3_VoxelyzeKernel.cu
+++ src/VX3/VX3_VoxelyzeKernel.cu
@@ -217,9 +217,11 @@ __device__ void VX3_VoxelyzeKernel::init() {
   right_a_x  = abs(right_max_x - right_min_x) + base_size.x * 2;
   a_y  = abs(max_y - min_y) + base_size.y * 2;
   a_z  = abs(max_z - min_z) + base_size.z * 2;
-  printf("Center right: (%d,%d,%d)", right_wall_center_x, right_wall_center_y, right_wall_center_z);
-  printf("Center left: (%d,%d,%d)", left_wall_center_x, left_wall_center_y, left_wall_center_z);
-  printf("Edges: (%d,%d,%d,%d)", right_a_x, left_a_x, a_y, a_z);
+  printf("base size: (%d,%d,%d)\n", base_size.x, base_size.y, base_size.z);
+  printf("numbers: (%d,%d)", num_left_voxels, num_right_voxels);
+  printf("Center right: (%d,%d,%d)\n", right_wall_center_x, right_wall_center_y, right_wall_center_z);
+  printf("Center left: (%d,%d,%d)\n", left_wall_center_x, left_wall_center_y, left_wall_center_z);
+  printf("Edges: (%d,%d,%d,%d)\n", right_a_x, left_a_x, a_y, a_z);
 }
 __device__ void VX3_VoxelyzeKernel::saveInitialPosition() {
     for (int i = 0; i < num_d_voxels; i++) {
