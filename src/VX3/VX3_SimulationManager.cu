1d65841f551f09bdba2c4faaec5fcaad50f08f2f
Federico Pigozzi <pigozzife@gmail.com>
2022-09-28 23:49:13 -0400

    cleanup

--- src/VX3/VX3_SimulationManager.cu
+++ src/VX3/VX3_SimulationManager.cu
@@ -30,7 +30,6 @@ __global__ void CUDA_Simulation(VX3_VoxelyzeKernel *d_voxelyze_3, int num_simula
     int thread_index = blockIdx.x * blockDim.x + threadIdx.x;
     if (thread_index < num_simulation) {
         VX3_VoxelyzeKernel *d_v3 = &d_voxelyze_3[thread_index];
-        d_v3->init();
         VX3_DistributedNeuralController* controller = new VX3_DistributedNeuralController(d_v3, d_v3->weights);
         if (d_v3->num_d_links == 0 and d_v3->num_d_voxels == 0) {
             printf(COLORCODE_BOLD_RED "No links and no voxels. Simulation %d (%s) abort.\n" COLORCODE_RESET, thread_index,
