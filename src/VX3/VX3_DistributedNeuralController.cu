11683fc2ff7401eb7a51fee7fd37922cd41800e0
Federico Pigozzi <pigozzife@gmail.com>
2022-10-16 17:12:41 -0400

    update vote with touching

--- src/VX3/VX3_DistributedNeuralController.cu
+++ src/VX3/VX3_DistributedNeuralController.cu
@@ -65,7 +65,7 @@ __device__ double VX3_DistributedNeuralController::updateVoxelTemp(VX3_Voxel* vo
     voxel->currSignals[dir] = voxel->outputs[2 + ((dir % 2 == 0) ? dir + 1 : dir - 1)];
   }
   if (firstRightContact && firstLeftContact) {
-    tempVotes->push_back({voxel->outputs[1], voxel->ix, voxel->iy, voxel->iz});
+    tempVotes->push_back({voxel->outputs[1], voxel->ix, voxel->iy, voxel->iz, (voxel->inputs[0] > 0.0) ? 1 : 0});
   }
   return voxel->outputs[0];
 }
