aad8deac7bd911be5d9d3120b8391de5edf6be31
Federico Pigozzi <pigozzife@gmail.com>
2022-10-20 22:26:19 -0400

    two walls

--- src/VX3/VX3_DistributedNeuralController.cu
+++ src/VX3/VX3_DistributedNeuralController.cu
@@ -64,7 +64,7 @@ __device__ double VX3_DistributedNeuralController::updateVoxelTemp(VX3_Voxel* vo
   for (int dir = 0; dir < NUM_SIGNALS; ++dir) {
     voxel->currSignals[dir] = voxel->outputs[2 + ((dir % 2 == 0) ? dir + 1 : dir - 1)];
   }
-  if (firstRightContact || firstLeftContact) {
+  if (firstRightContact && firstLeftContact) {
     tempVotes->push_back({voxel->outputs[1], voxel->ix, voxel->iy, voxel->iz, (voxel->inputs[1] > 0.0) ? 1 : 0});
   }
   return voxel->outputs[0];
@@ -79,7 +79,7 @@ __device__ void VX3_DistributedNeuralController::printVotes(VX3_VoxelyzeKernel* 
 }
 
 __device__ void VX3_DistributedNeuralController::vote(void) {
-  if (!firstLeftContact && !firstRightContact) {
+  if (!firstLeftContact || !firstRightContact) {
     return;
   }
   int numPos = 0;
