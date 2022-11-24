1c93b8afe1d02e95b13c5a38e860d75af44bf87e
Federico Pigozzi <pigozzife@gmail.com>
2022-11-24 13:39:54 +0100

    from int to double

--- src/VX3/VX3_VoxelyzeKernel.cu
+++ src/VX3/VX3_VoxelyzeKernel.cu
@@ -565,10 +565,10 @@ __device__ void VX3_VoxelyzeKernel::computeFitness(VX3_DistributedNeuralControll
         sensing_score += 0.5;
       }*/
       if (is_passable == 1) {
-        sensing_score += controller->votes->get(i) / 17;
+        sensing_score += controller->votes->get(i) / 17.0;
       }
       else {
-        sensing_score += (17 - controller->votes->get(i)) / 17;
+        sensing_score += (17.0 - controller->votes->get(i)) / 17.0;
       }
       //sensing_score += (controller->votes->get(i) == is_passable) ? 1.0 : 0.0;
     }
