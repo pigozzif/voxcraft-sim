c1b5706086b1908d146aba5cbd0d24392103a7c9
Federico Pigozzi <pigozzife@gmail.com>
2022-12-04 13:18:40 +0100

    fix bug

--- src/VX3/VX3_DistributedNeuralController.h
+++ src/VX3/VX3_DistributedNeuralController.h
@@ -23,7 +23,7 @@ public:
   __device__ VX3_MLP(const int numInputs, const int numOutputs, double* weights);
   __device__ ~VX3_MLP(void);
   
-  __device__ void apply(VX3_Voxel* voxel) const;
+  __device__ void apply(VX3_Voxel* voxel);
   __device__ void hebbianUpdate(VX3_Voxel* voxel);
   __device__ inline int getNumInputs(void) const { return numInputs; }
   __device__ inline int getNumOutputs(void) const { return numOutputs; }
