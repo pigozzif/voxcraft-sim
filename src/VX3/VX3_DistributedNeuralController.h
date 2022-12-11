0726d5289cbe93413b67123cb4749b02942a2d09
Federico Pigozzi <pigozzife@gmail.com>
2022-12-11 20:47:31 +0100

    rnn members

--- src/VX3/VX3_DistributedNeuralController.h
+++ src/VX3/VX3_DistributedNeuralController.h
@@ -6,6 +6,7 @@
 
 #define NUM_SENSORS 5
 #define NUM_SIGNALS 12
+#define NUM_HIDDEN 6
 
 class VX3_VoxelyzeKernel;
 
@@ -20,24 +21,23 @@ struct Vote {
 class VX3_MLP
 {
 public:
-  __device__ VX3_MLP(const int numInputs, const int numOutputs, double* weights);
+  __device__ VX3_MLP(const int numInputs, const int numOutputs, int numHidden, double* weights_x, double* weights_h, double* weights_y);
   __device__ ~VX3_MLP(void);
   
   __device__ void apply(VX3_Voxel* voxel) const;
-  __device__ inline int getNumInputs(void) const { return numInputs; }
-  __device__ inline int getNumOutputs(void) const { return numOutputs; }
-
-  __device__ double* getWeights(void) const { return weights; };
   
   int numInputs;
   int numOutputs;
-  double* weights;
+  int numHidden;
+  double* weights_x;
+  double* weights_h;
+  double* weights_y;
 };
 
 class VX3_DistributedNeuralController
 {
 public:
-  __device__ VX3_DistributedNeuralController(VX3_VoxelyzeKernel* kernel, double* weights, int random_seed=0);  
+  __device__ VX3_DistributedNeuralController(VX3_VoxelyzeKernel* kernel, double* weights_x, double* weights_h, double* weights_y, int random_seed=0);  
   __device__ double updateVoxelTemp(VX3_Voxel* voxel, VX3_VoxelyzeKernel* kernel);
   __device__ void updateLastSignals(VX3_VoxelyzeKernel* kernel);
   __device__ void getLastSignals(VX3_Voxel* voxel) const;
