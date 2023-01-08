7f010401cf10a81d1772ec979e1ccbc91d7e35fe
pigozzif <pigozzife@gmail.com>
2023-01-08 12:33:55 +0100

    Revert "smooth fitness"
    This reverts commit 675fa682365c443142fc0a7c9c97e1596cc92cab.

--- src/VX3/VX3_DistributedNeuralController.cu
+++ src/VX3/VX3_DistributedNeuralController.cu
@@ -121,8 +121,17 @@ __device__ void VX3_DistributedNeuralController::vote(void) {
       numNeg += 1;
     }
   }
-  votes->push_back(numPos);
-  //votes->push_back((numPos >= numNeg) ? 1 : 0);
+  /*if (numPos == 0) {
+    votes->push_back(0);
+  }
+  else if (numNeg == 0) {
+    votes->push_back(1);
+  }
+  else {
+    votes->push_back(-1);
+  }*/
+  //votes->push_back(numPos);
+  votes->push_back((numPos >= numNeg) ? 1 : 0);
   tempVotes->clear();
 }
 
